24211830e8d33bc216d75778762d668f9c693d68
Maciej Lichoń <maciek.lichon@gmail.com>
2021-12-04 21:41:53 +0100

    Add functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return 'kamień';
+  }
+  else if (argMoveId == 2) {
+    return 'papier';
+  }
+  else if (argMoveId == 3) {
+    return 'nożyce';
+  }
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+  printMessage('Komputer: ' + argComputerMove + ', Gracz: ' + argPlayerMove)
+  if (argComputerMove === argPlayerMove) {
+    return 'Remis';
+  }
+  else if ( (argPlayerMove === 'kamień' && argComputerMove === 'papier') || (argPlayerMove === 'papier' && argComputerMove === 'nożyce') || (argPlayerMove === 'nożyce' && argComputerMove === 'kamień') ) {
+    return 'Komputer wygrał.';
+  }
+  else {
+    return 'Gracz wygrał.';
+  }
+}
+
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Komputer wylosował: ' + randomNumber);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = getMoveName(randomNumber);
+console.log('Komputer gra ' + computerMove);
 
+/*
 let computerMove = 'nieznany ruch';
 
 if (randomNumber === 1) {
@@ -13,14 +45,15 @@ else if (randomNumber === 2) {
 else if (randomNumber === 3) {
   computerMove = 'nożyce';
 }
-
-printMessage('Komputer: ' + computerMove);
-
+*/
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = getMoveName(playerInput);
+console.log('Gracz gra ' + playerMove);
+
+/*
 let playerMove = 'nieznany ruch';
 
 if(playerInput === '1') {
@@ -32,12 +65,14 @@ else if (playerInput === '2') {
 else if (playerInput === '3') {
   playerMove = 'nożyce';
 }
+*/
 
-printMessage('Gracz: ' + playerMove);
+if ( (playerMove != 'nieznany ruch') ) { 
+  let result = displayResult(computerMove, playerMove);
+  printMessage(result);
+}
 
-
-let result = '';
-
+/*
 if (playerMove === 'nieznany ruch') {
   printMessage('Zły input, wpisz 1, 2 lub 3');
 }
@@ -49,8 +84,7 @@ else {
     result = 'Komputer wygrał';
   }
   else {
-    result = 'Gracz wygrał'
+    result = 'Gracz wygrał';
   }
 }
-
-printMessage(result)
+*/
